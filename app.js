b3a010b9fc5f540513ed77dd15f10a020c480a05
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-10-31 22:16:07 -0300

    deploy Heroku

--- app.js
+++ app.js
@@ -73,11 +73,16 @@ app.post("/compose", function(req, res) {
 })
 
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000
+}
+
+app.listen(port);
 
 
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
