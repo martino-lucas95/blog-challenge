6ef9c640f08bd70672c4c5f901d43ce58ddfd783
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-11-04 18:48:05 -0300

    Fix postId Error

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const Post = mongoose.model("Post", postSchema);
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+// let posts = [];
 
 app.get("/", function(req, res){
 
@@ -37,36 +37,36 @@ app.get("/", function(req, res){
 });
 
 
-app.get("/compose", function (req, res) {
-  res.render("compose");    
-
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
 app.post("/compose", function(req, res){
-
-  const post = new Post ({
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
-  post.save(function (err) {
-    if(!err){
-      res.redirect("/");
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
   });
-
-
 });
 
 app.get("/posts/:postId", function(req, res){
 
   const requestedPostId = req.params.postId;
-  
     Post.findOne({_id: requestedPostId}, function(err, post){
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
+      if(!err){
+        res.render("post", {
+          title: post.title,
+          content: post.content
+          })
+        } else{
+        console.log(err);
+      }
     });
   
   });
@@ -88,16 +88,16 @@ app.get("/about", function (req, res) {
 
 //Heroku
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-  port = 3000
-}
+// let port = process.env.PORT;
+// if(port == null || port == ""){
+//   port = 3000
+// }
 
-app.listen(port);
-
-
+// app.listen(port);
 
 
-// app.listen(3000, function() {
-//   console.log("Server has started successfully");
-// });
+
+
+app.listen(3000, function() {
+  console.log("Server has started successfully");
+});
